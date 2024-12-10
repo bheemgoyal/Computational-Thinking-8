e78d299e1ab1d4e5ac5311bd670e1b84a25ebf68
bheemgoyal <bheemgoyal@seattleacademy.org>
2024-12-10 20:16:22 +0000

    day 4

--- Projects/Project1.py
+++ Projects/Project1.py
@@ -7,8 +7,18 @@ stage = StageClass()
 
 
 
-stage.set_background("tree")
-q1 = codesters.Square(100, 100, 200, 'green')
+stage.set_background("nature.png")
+
+q1 = codesters.Circle(100, 100, 200, 'green')
 q1 = codesters.Square(100, -100, 200, 'purple')
 q1 = codesters.Square(-100, 100, 200, 'red')
 q1 = codesters.Square(-100, -100, 200, 'blue')
+
+s1 = codesters.Sprite("soccerball.gif", 100, 100)
+s2 = codesters.Sprite("cardinal", -100, 100)
+s1.set_size(2)
+s3 = codesters.Sprite("zelda.jpg", -100, -100)
+s3.set_size(0.35)
+s4 = codesters.Sprite("ramen.jpeg", 100, -100)
+s4.set_size(0.7)
+message1 = codesters.Text("I like food and soccer",0,-220,"black")
