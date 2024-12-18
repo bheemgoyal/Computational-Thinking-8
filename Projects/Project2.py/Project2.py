0ed348b3f8d1f203ec927173b3b7a7cb466197ad
bheemgoyal <bheemgoyal@seattleacademy.org>
2024-12-18 18:45:26 +0000

    Personality quiz

--- Projects/Project2.py/Project2.py
+++ Projects/Project2.py/Project2.py
@@ -1,17 +1,60 @@
-###############################################
-### SETUP ###
-import codesters
-from codesters import StageClass
-stage = StageClass()
-###############################################
+# Beginning of quiz
+christmas_points = 0
+halloween_points = 0
 
 
-while True:
-    word = input("What do you think grandma likes? ")
+# Middle of quiz
+answer = input("Do you prefer spending time with A) friends, or B) family?")
+if answer == "A":
+    halloween_points += 1
+elif answer == "B":
+    christmas_points += 1
 
-    if "z" in word:
-        print(f"Grandma doesn't like {word}!")
-    else:
-        print(f"Grandma likes {word}")
 
-    print("")
+answer = input("Do you prefer A) candy, or B) cookies?")
+if answer == "A":
+    halloween_points += 1
+elif answer == "B":
+    christmas_points += 1
+
+
+answer = input("Do you prefer A) scary movies, or B) christmas movies?")
+if answer == "A":
+    halloween_points += 1
+elif answer == "B":
+    christmas_points += 1
+
+
+answer = input("Do you prefer A) costumes, or B) Pjamas?")
+if answer == "A":
+    halloween_points += 1
+elif answer == "B":
+    christmas_points += 1
+
+
+answer = input("Do you prefer A) haunted houses, or B) decorated christmas trees?")
+if answer == "A":
+    halloween_points += 1
+elif answer == "B":
+    christmas_points += 1
+
+
+answer = input("Do you prefer A) spooky stuff, or B) jolly stuff?")
+if answer == "A":
+    halloween_points += 1
+elif answer == "B":
+    christmas_points += 1
+
+
+answer = input("Do you prefer A) exterior decorations, or B) interior decorations?")
+if answer == "A":
+    halloween_points += 1
+elif answer == "B":
+    christmas_points += 1
+
+
+# end of quiz:
+if halloween_points > 3:
+    print("You are a halloween person")
+elif christmas_points > 3:
+    print("You are a christmas person")
